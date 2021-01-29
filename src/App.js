5b2aeb164321b03426721a0142fcc8583bba443d
judy1230 <50729317+judy1230@users.noreply.github.com>
2021-01-29 13:43:34 +0800

    function is ready

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useCallback } from 'react'
 import styled from '@emotion/styled'
 import { ThemeProvider } from '@emotion/react'
 import { getMoment } from './utils/helpers'
-import { findLocation } from './utils/helpers'
 import WeatherCard from './views/WeatherCard'
-// step 1 : 匯入useWeatherAPI匯入useWeatherAPI
 import useWeatherAPI from './hooks/useWeatherAPI'
+import WeatherSetting from './views/WeatherSetting'
+
 const AUTHORIZATION_KEY = 'CWB-6B0FF102-FDE8-40C3-B2CA-CECA1E4E67E5'
 const LOCATION_NAME = '宜蘭'
 const BASE_URL = 'opendata.cwb.gov.tw/api'
@@ -46,7 +46,6 @@ const Container = styled.div`
 
 function App() {
   const [currentTheme, setCurrentTheme] = useState('light')
-  //step 2 : 使用 useWeatherAPI使用 useWeatherAPI
   const [weatherElement, fetchData] = useWeatherAPI({
     locationName: LOCATION_NAME,
     cityName: LOCATION_NAME_FORECAST,
@@ -55,19 +54,28 @@ function App() {
     currentWeatherUrl: CURRENT_WEATHER_URL,
     baseUrl: BASE_URL
   })
+  //step 4 : 定義 currentPage 這個state, 預設值是WeatherCard
+  const [currentPage, setCurrentPage] = useState('WeatherCard')
 
   const moment = getMoment('宜蘭縣')
   //根據 moment 決定要使用亮色或暗色主題
   useEffect(() => {
-    //console.log('moment', moment)
     setCurrentTheme(moment === 'day' ? 'light' : 'dark')
   }, [moment])
-  
+
 
   return (
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
-        <WeatherCard weatherElement={weatherElement} moment={moment} fetchData={ fetchData }/>
+        {/* step 5 : 利用條件轉譯的方式決定要呈現哪個元件 */}
+        {currentPage === 'WeatherCard' && (
+          <WeatherCard weatherElement={weatherElement} moment={moment} fetchData={fetchData} />
+        )}
+        {/* step 3 : 放入WeatherSetting元件
+        <WeatherSetting /> */}
+        {/* step 6 : 條件轉譯帶入WeatherSetting元件中 */}
+        {currentPage === 'WeatherSetting' && <WeatherSetting />}
+        
       </Container>
     </ThemeProvider>
   )
